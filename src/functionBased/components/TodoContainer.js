dfaacf5773c8a9d979b5adfaad7f89b583e34d2e
maen <maenkh@yahoo.com>
2022-06-06 21:05:32 +0300

    routers

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,81 +1,97 @@
 import React, { useState, useEffect } from "react"
+import { Routes, BrowserRouter as Route } from 'react-router-dom';
+
 import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
+import Navbar from "./Navbar"
 import { v4 as uuidv4 } from "uuid"
+
+import About from "../pages/About"
+import NotMatch from "../pages/NotMatch"
 
 const TodoContainer = () => {
 
-    const [todos, setTodos] = useState(getInitialTodos());
+  const [todos, setTodos] = useState(getInitialTodos());
 
-    function getInitialTodos() {
-        // getting stored items
-        const temp = localStorage.getItem("todos")
-        const savedTodos = JSON.parse(temp)
-        return savedTodos || []
-      }
-    useEffect(() => {
-        // storing todos items
-        const temp = JSON.stringify(todos)
-        localStorage.setItem("todos", temp)
-      }, [todos])
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem("todos")
+    const savedTodos = JSON.parse(temp)
+    return savedTodos || []
+  }
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos)
+    localStorage.setItem("todos", temp)
+  }, [todos])
 
-      const handleChange = id => {
-        setTodos(prevState =>
-          prevState.map(todo => {
-            if (todo.id === id) {
-              return {
-                ...todo,
-                completed: !todo.completed,
-              }
-            }
-            return todo
-          })
-        )
-      }
-
-    const delTodo = id => {
-        setTodos([
-            ...todos.filter(todo =>{
-                return todo.id !== id
-            })
-        ])
-    }
-
-    const addTodoItem = title => {
-        const newTodo = {
-          id: uuidv4(),
-          title: title,
-          completed: false,
+  const handleChange = id => {
+    setTodos(prevState =>
+      prevState.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
         }
-        setTodos([...todos, newTodo])
-      }
-    
-      const setUpdate = (updatedTitle, id) => {
-        setTodos(
-          todos.map(todo => {
-            if (todo.id === id) {
-              todo.title = updatedTitle
-            }
-            return todo
-          })
-        )
-      }
-    
-      return (
-        <div className="container">
-          <div className="inner">
-            <Header />
-            <InputTodo addTodoProps={addTodoItem} />
-            <TodosList
-              todos={todos}
-              handleChangeProps={handleChange}
-              deleteTodoProps={delTodo}
-              setUpdate={setUpdate}
-            />
-          </div>
-        </div>
-      )
+        return todo
+      })
+    )
+  }
+
+  const delTodo = id => {
+    setTodos([
+      ...todos.filter(todo => {
+        return todo.id !== id
+      })
+    ])
+  }
+
+  const addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
     }
-    
-    export default TodoContainer
+    setTodos([...todos, newTodo])
+  }
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      })
+    )
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route>
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={addTodoItem} />
+              <TodosList
+                todos={todos}
+                handleChangeProps={handleChange}
+                deleteTodoProps={delTodo}
+                setUpdate={setUpdate}
+              />
+            </div>
+          </div>
+        </Route>
+        <Route>
+          <About />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default TodoContainer
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter  } from "react-router-dom"
 //component file
+
 import TodoContainer from "./functionBased/components/TodoContainer"
 import "./functionBased/App.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter >
+      <React.Fragment>
+        <TodoContainer />
+      </React.Fragment>
+    </BrowserRouter >
   </React.StrictMode>
 )
