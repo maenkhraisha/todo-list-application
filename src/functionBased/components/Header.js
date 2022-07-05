2871af03ede98850478dd1bb691dced2c287befb
maen <maenkh@yahoo.com>
2022-07-05 17:06:26 +0300

    finish styling

--- src/functionBased/components/Header.js
+++ src/functionBased/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import logo from '../img/logo.svg';
+import logoAnimated from '../img/logo-animated.gif';
+
 const Header = () => {
   const headerStyle = {
     padding: '20px 0',
     lineHeight: '1.5em',
   };
   return (
-    <header style={headerStyle}>
-      <div class="one">
-        <h1>To Do</h1>
+    <header className="header">
+      <img className="logo" src={logoAnimated} />
+      <div className="tilte-logo-container">
+        <div className="one">
+          <h1>To Do</h1>
+        </div>
+        <img className="logoicon" src={logo} />
       </div>
     </header>
   );
