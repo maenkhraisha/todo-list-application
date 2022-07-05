34b8fe16bf2e5e1afde931a557d5705e85c76a3c
maen <maenkh@yahoo.com>
2022-07-05 18:41:37 +0300

    edit route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 //component file
 
-import About from './functionBased/pages/About';
-import NotMatch from './functionBased/pages/NotMatch';
 
 import TodoContainer from './functionBased/components/TodoContainer';
 import './functionBased/App.css';
@@ -13,11 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotMatch />} />
-        <Route path="/" element={<TodoContainer />} />
-      </Routes>
+     <TodoContainer />
     </Router>
   </React.StrictMode>
 );
